16e1772dd35e1ce5ca0fea21793b3cb9dae6ca0f
CMFell <cmf21@st-andrews.ac.uk>
2020-08-11 00:25:37 +0100

    Debug merge add testcase 4 with 15 knots

--- R/return.reg.spline.fit.2d.R
+++ R/return.reg.spline.fit.2d.R
@@ -61,7 +61,6 @@
   # }
   # 
   print('branch is add_loop_to_drop')
-  
   # split out spline parameter object into its pieces
   knotDist <- splineParams[[1]]$knotDist
   radii <- splineParams[[1]]$radii
